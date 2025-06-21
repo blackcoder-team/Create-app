a921d61af06a1c113b62e9f45b2008f02015bcf3
qoramen <tojiboyevmurodbbek@gmail.com>
2025-06-21 14:34:11 +0500

    Nav

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -2,7 +2,23 @@ import './nav.css'
 
 const Nav = () => {
   return (
-    <div id='nav'></div>
+    <div id='nav'>
+      <div className="container">
+        <div className="row pt-4 pb-4 d-flex justify-content-between">
+          <div className="logo d-inline-block w-auto">
+            <h1>Create<span>.</span></h1>
+          </div>
+          <ul className='d-inline-block w-auto d-flex m-0'>
+            <li><a href="">Home</a></li>
+            <li><a href="">Work</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
