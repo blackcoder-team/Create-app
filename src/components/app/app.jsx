25c9978e43f597c27d27aee58071df0cf508a0ef
qoramen <tojiboyevmurodbbek@gmail.com>
2025-06-21 12:17:10 +0500

    1st state

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,24 @@
 import { Nav } from "../navbar/nav";
-import { Hero } from "../hero/hero";
-import { Info } from "../info/info";
-import { Works } from "../works/works";
-import { Feature } from "../feature/feature";
-import { Testimonial } from "../testimonial/testimonial";
-import { Service } from "../service";
-import { About } from "../about/about";
-import { Team } from "../team/team";
-import { Blog } from "../blog/blog";
-import { Contact } from "../contact/contact";
-import { Footer } from "../footer/footer";
+// import { Hero } from "../hero/hero";
+// import { Info } from "../info/info";
+// import { Works } from "../works/works";
+// import { Feature } from "../feature/feature";
+// import { Testimonial } from "../testimonial/testimonial";
+// import { Service } from "../service";
+// import { About } from "../about/about";
+// import { Team } from "../team/team";
+// import { Blog } from "../blog/blog";
+// import { Contact } from "../contact/contact";
+// import { Footer } from "../footer/footer";
 
 import './app.css'
 
-const app = () => {
+const App = () => {
     return (
         <>
             <Nav />
         </>
     )
 }
+
+export default App
