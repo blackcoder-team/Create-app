6ce143d2d404b43b7d24c9bb8b4309efbe034277
jakhonngir <bahromovam843@gmail.com>
2025-06-22 15:56:05 +0500

    our

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,14 @@
-import { Nav } from "../navbar/nav";
-// import { Hero } from "../hero/hero";
-// import { Info } from "../info/info";
+import Nav from "../navbar/nav";
+import Hero from "../hero/hero";
+import Info from "../info/info";
 // import { Works } from "../works/works";
 // import { Feature } from "../feature/feature";
 // import { Testimonial } from "../testimonial/testimonial";
-// import { Service } from "../service";
-// import { About } from "../about/about";
-// import { Team } from "../team/team";
-// import { Blog } from "../blog/blog";
-// import { Contact } from "../contact/contact";
+import Service from "../service/service";
+import About from "../about/about";
+import Team from "../team/team";
+import Blog from "../blog/blog";
+import Contact from "../contact/contact";
 // import { Footer } from "../footer/footer";
 
 import './app.css'
@@ -16,7 +16,13 @@ import './app.css'
 const App = () => {
     return (
         <>
-            <Nav />
+            <Hero />
+            <Info />
+            <Service />
+            <About />
+            <Team />
+            <Blog />
+            <Contact />
         </>
     )
 }
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,14 @@
+import './hero.css'
+
 const Hero = () => {
   return (
-    <div>heroes</div>
+    <div className='hero'>
+      <div className='hero_text'>
+        <p className='hero_text_title'>We Love To Build Mobile Apps|</p>
+        <p className='hero_text_p'>Free Web Template by Colorlib</p>
+        <button className='hero_text_btn'>Watch Video</button>
+      </div>
+    </div>
   )
 }
 
